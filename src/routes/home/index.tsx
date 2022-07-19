401ecf3f8ff0fdcdcaea486812fc203c9c4533c7
Olivier Basille <obasille@hotmail.com>
2022-07-19 23:16:42 +0200

    chore: let use select which animation plays on die based on ranking result

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -8,158 +8,360 @@ import {
   ConnectionEventData,
   RollState,
   Color,
-  EditDataSet,
-  EditAnimationRainbow,
-  AppDataSet,
   loadAppDataSet,
+  AppDataSet,
+  EditAnimation,
+  EditProfile,
+  EditRule,
+  EditConditionIdle,
+  EditActionPlayAnimation,
+  EditAnimationSimple,
+  PixelRollStateValues,
 } from "@systemic-games/pixels-library";
-import { useEffect, useState } from "preact/hooks";
+import { useCallback, useEffect, useRef, useState } from "preact/hooks";
 
-const log = console.log;
-
-let niceProfileAppDataSet: AppDataSet;
-fetch("./assets/nice-profile.json")
-  .then((res) => res.json())
-  .then((data) => {
-    log("data:", data);
-    niceProfileAppDataSet = loadAppDataSet(data);
-  });
-
-interface PixelInfoBoxParams {
-  pixel: Pixel;
+async function loadAppDataSetFromJson(jsonFilename: string) {
+  const res = await fetch(`./assets/${jsonFilename}`);
+  const data = await res.json();
+  console.log("Loaded profiles:", data);
+  return loadAppDataSet(data);
 }
 
-const PixelInfoBox: FunctionalComponent<PixelInfoBoxParams> = ({ pixel }) => {
+interface PixelInfoBoxProps {
+  pixel: Pixel;
+  onRolled?: (pixel: Pixel, face: number) => void;
+  onRemove?: (pixel: Pixel) => void;
+}
+
+const PixelInfoBox: FunctionalComponent<PixelInfoBoxProps> = ({
+  pixel,
+  onRolled,
+  onRemove,
+}) => {
   const [lastConnEv, setLastConnEv] = useState<ConnectionEvent>(
-    ConnectionEventValues.Disconnected
+    ConnectionEventValues.Connecting //TODO assume connecting by default
   );
   const [lastRoll, setLastRoll] = useState(0);
 
   useEffect(() => {
-    pixel.addEventListener(
-      "connectionEvent",
-      (ev: CustomEvent<ConnectionEventData>) => {
-        const connEv = (ev.detail as ConnectionEventData).event;
-        setLastConnEv(connEv);
-        if (connEv === "ready") {
-          const update = async (): Promise<void> => {
-            // Get some info
-            const rollState = await pixel.getRollState();
-            log(
-              `=> roll state: ${rollState.state}, ` +
-                `face ${rollState.faceIndex + 1}`
-            );
-            const battery = await pixel.getBatteryLevel();
-            log(`=> battery: ${battery.level}, ${battery.voltage}v`);
-            const rssi = await pixel.getRssi();
-            log(`=> rssi: ${rssi}`);
+    const onConnectionEvent = (ev: CustomEvent<ConnectionEventData>) => {
+      const connEv = (ev.detail as ConnectionEventData).event;
+      setLastConnEv(connEv);
+      if (connEv === "ready") {
+        const update = async () => {
+          // Get some info
+          const battery = await pixel.getBatteryLevel();
+          console.log(
+            `${pixel.name} => battery: ${battery.level}, ${battery.voltage}v`
+          );
+          const rssi = await pixel.getRssi();
+          console.log(`${pixel.name} => rssi: ${rssi}`);
+          const rollState = await pixel.getRollState();
+          const face = rollState.faceIndex + 1;
+          console.log(
+            `${pixel.name} => initial roll state: ${rollState.state}, face ${face}`
+          );
+          setLastRoll(face);
+        };
 
-            // Add listener to get notified when the Pixel roll state changes
-            pixel.addEventListener(
-              "messageRollState",
-              (ev: CustomEvent<MessageOrType>) => {
-                const msg = ev.detail as RollState;
-                log(`=> roll state: ${msg.state}, face ${msg.faceIndex + 1}`);
-                setLastRoll(msg.faceIndex + 1);
-              }
-            );
-          };
-
-          update().catch((error) => {
-            log(error);
-          });
-        }
+        update().catch((error) => {
+          console.log(error);
+        });
       }
-    );
-  }, [pixel]);
+    };
 
-  const blink = async (): Promise<void> => {
+    const onRollEvent = (ev: CustomEvent<MessageOrType>) => {
+      const msg = ev.detail as RollState;
+      if (msg.state === PixelRollStateValues.OnFace) {
+        const face = msg.faceIndex + 1;
+        console.log(`${pixel.name} => roll state: ${msg.state}, face ${face}`);
+        setLastRoll(face);
+        onRolled?.(pixel, face);
+      }
+    };
+
+    // Add listeners to get notified on connection and roll events
+    pixel.addEventListener("connectionEvent", onConnectionEvent);
+    pixel.addEventListener("messageRollState", onRollEvent);
+
+    return () => {
+      pixel.removeEventListener("connectionEvent", onConnectionEvent);
+      pixel.removeEventListener("messageRollState", onRollEvent);
+    };
+  }, [pixel, onRolled]);
+
+  const blink = async () => {
     await pixel.blink(Color.red, 3);
   };
 
-  const rainbow = async (): Promise<void> => {
-    const editDataSet = new EditDataSet();
-    editDataSet.animations.push(
-      new EditAnimationRainbow({
-        duration: 3,
-        count: 2,
-        fade: 0.5,
+  // const rainbow = async () => {
+  //   const editDataSet = new EditDataSet();
+  //   editDataSet.animations.push(
+  //     new EditAnimationRainbow({
+  //       duration: 3,
+  //       count: 2,
+  //       fade: 0.5,
+  //     })
+  //   );
+  //   pixel.playTestAnimation(editDataSet.toDataSet());
+  // };
+
+  return (
+    <p>
+      <text class={style.boldText}>Die: {pixel.name}</text>
+      <br />
+      <text>Status: {lastConnEv}</text>
+      <br />
+      <text>Last roll: {lastRoll}</text>
+      <p />
+      <button onClick={blink}>Blink</button>
+      {/* <button onClick={rainbow}>Rainbow</button> */}
+      {onRemove ? (
+        <button onClick={() => onRemove(pixel)}>Remove</button>
+      ) : (
+        <></>
+      )}
+    </p>
+  );
+};
+
+interface SelectAnimationProps {
+  animations: EditAnimation[];
+  onAnimationChange: (value?: EditAnimation) => void;
+}
+
+const SelectAnimation: FunctionalComponent<SelectAnimationProps> = ({
+  animations,
+  onAnimationChange,
+}) => {
+  const [index, setIndex] = useState(-1);
+  return (
+    <select
+      value={index}
+      onChange={(e) => {
+        const i = Number(e.currentTarget.value);
+        setIndex(i);
+        onAnimationChange(animations[i]);
+      }}
+    >
+      {animations.map((p, i) => (
+        <option key={i} value={i}>
+          {p.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+interface DemoProps {
+  defaultAppDataSet: AppDataSet;
+}
+
+const Demo: FunctionalComponent<DemoProps> = ({ defaultAppDataSet }) => {
+  const [pixels, setPixels] = useState<Pixel[]>([]);
+  const selectedAnimationsRef = useRef<(EditAnimation | undefined)[]>([]);
+  const rollsRef = useRef<(number | undefined)[]>([]);
+
+  const connect = async () => {
+    // Ask user to select a Pixel
+    try {
+      resetRolls();
+      const pixel = await Pixel.requestPixel();
+      setPixels((pixels) => {
+        if (!pixels.includes(pixel)) {
+          return [...pixels, pixel];
+        }
+        return pixels;
+      });
+      await pixel.connect(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const disconnect = async (pixel: Pixel) => {
+    resetRolls();
+    setPixels((pixels) => {
+      if (pixels.includes(pixel)) {
+        return pixels.filter((p) => p !== pixel);
+      }
+      return pixels;
+    });
+    pixel.disconnect();
+  };
+
+  const resetRolls = () => {
+    rollsRef.current.length = 0;
+  };
+
+  const onRolled = useCallback(
+    (pixel: Pixel, face: number) => {
+      console.log(pixels);
+      const index = pixels.indexOf(pixel);
+      if (index >= 0) {
+        rollsRef.current[index] = face;
+        const rolls = rollsRef.current.filter((f) => !!f) as number[];
+        console.log(rolls);
+        if (pixels.length === rolls.length) {
+          const sortedDice = [...pixels];
+          sortedDice.sort((p0, p1) => {
+            const i0 = pixels.indexOf(p0);
+            const i1 = pixels.indexOf(p1);
+            return rolls[i1] - rolls[i0];
+          });
+          console.log(rolls);
+          console.log(
+            `All dice rolled: ${sortedDice.map((p) => p.name).join(", ")}`
+          );
+          resetRolls();
+          Promise.allSettled(
+            sortedDice.map((pixel, i) => {
+              console.log(`Playing ${i} on ${pixel.name}`);
+              return pixel.playInstantAnimation(i, true);
+            })
+          ).catch((error) => console.error(error));
+        }
+      } else {
+        console.error(`Got rolled on unknown die: ${pixel.name}`);
+      }
+    },
+    [pixels]
+  );
+
+  const program = async (defaultAppDataSet: AppDataSet): Promise<boolean> => {
+    if (pixels.length) {
+      const animations = pixels.map((_, i) => {
+        const anim = selectedAnimationsRef.current[i];
+        return (
+          anim ??
+          //TODO use Color.dimmedRed
+          new EditAnimationSimple({
+            duration: 1,
+            color: Color.fromBytes(100, 0, 0),
+            count: 2,
+          })
+        );
+      });
+      const profile = new EditProfile();
+      profile.rules.push(
+        new EditRule(
+          new EditConditionIdle(),
+          animations.map((a) => {
+            return new EditActionPlayAnimation(a);
+          })
+        )
+      );
+      //TODO add extractAnimations() so to not create a dummy profile
+      const dataSet = new AppDataSet({
+        patterns: [...defaultAppDataSet.patterns],
+        animations,
+        profiles: [profile],
       })
-    );
-    pixel.playTestAnimation(editDataSet.toDataSet());
+        .extractForProfile(profile)
+        .toDataSet();
+      const results = await Promise.allSettled(
+        pixels.map(async (pixel) => {
+          try {
+            await pixel.transferInstantAnimations(dataSet);
+          } catch (error) {
+            console.error(error);
+            throw error;
+          }
+        })
+      );
+      resetRolls();
+      return results.every((r) => r.status === "fulfilled");
+    }
+    console.log("No dice to program");
+    return true;
   };
 
-  const instant = async (): Promise<void> => {
-    // const editDataSet = new EditDataSet();
-    // editDataSet.animations.push(
-    //   new EditAnimationRainbow({
-    //     duration: 3,
-    //     count: 2,
-    //     fade: 0.5,
-    //   })
-    // );
-    // editDataSet.animations.push(
-    //   new EditAnimationSimple({
-    //     duration: 3,
-    //     color: Color.magenta,
-    //     count: 2,
-    //     fade: 0.5,
-    //   })
-    // );
-    await pixel.transferInstantAnimations(
-      niceProfileAppDataSet
-        .extractForProfile(niceProfileAppDataSet.profiles[0])
-        .toDataSet()
-    );
+  const onAnimationChange = (slotIndex: number, animation?: EditAnimation) => {
+    selectedAnimationsRef.current[slotIndex] = animation;
   };
 
-  const playInstant = async (animIndex: number): Promise<void> => {
-    await pixel.playInstantAnimation(animIndex);
+  const testAnimation = async (slotIndex: number) => {
+    await Promise.allSettled(
+      pixels.map((pixel) => pixel.playInstantAnimation(slotIndex, true))
+    );
   };
 
   return (
     <>
-      <p>Die: {pixel.name}</p>
-      <p>Last conn ev: {lastConnEv}</p>
-      <p>Last roll: {lastRoll}</p>
-      <p>Pixel Id: 0x{pixel.pixelId.toString(16).toLocaleUpperCase()}</p>
-      <button onClick={blink}>Blink</button>
-      <p />
-      <button onClick={rainbow}>Rainbow</button>
-      <p />
-      <button onClick={instant}>Instant</button>
-      <button onClick={(): Promise<void> => playInstant(0)}>Play #0</button>
-      <button onClick={(): Promise<void> => playInstant(1)}>Play #1</button>
-      <button onClick={(): Promise<void> => playInstant(2)}>Play #2</button>
+      <button onClick={connect}>Connect To Pixel</button>
+      <p>
+        {pixels.length
+          ? `Pixels: ${pixels.map((p) => p.name).join(", ")}`
+          : "Click on the button to connect to a Pixel die"}
+      </p>
+      {pixels.map((_, i) => (
+        <p key={i}>
+          <text>{`Rank #${i + 1}: `}</text>
+          <SelectAnimation
+            animations={defaultAppDataSet.animations}
+            onAnimationChange={(a) => onAnimationChange(i, a)}
+          />
+          <button onClick={() => testAnimation(i)}>Test</button>
+        </p>
+      ))}
+      {pixels.length ? (
+        <>
+          <p />
+          <button onClick={() => program(defaultAppDataSet)}>
+            {`Program Dice (${pixels.length})`}
+          </button>
+          <p />
+          <p />
+          <text class={style.boldText}>Dice List:</text>
+          {pixels.map((pixel) => (
+            <PixelInfoBox
+              key={pixel}
+              pixel={pixel}
+              onRolled={onRolled}
+              onRemove={disconnect}
+            />
+          ))}
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
 
 const Home: FunctionalComponent = () => {
-  const [pixels, setPixels] = useState<Pixel[]>([]);
+  const [defaultAppDataSet, setDefaultAppDataSet] = useState<
+    AppDataSet | undefined
+  >();
+  // const [niceProfileDataSet, setNiceProfileDataSet] = useState<
+  //   DataSet | undefined
+  // >();
 
-  const connect = async (): Promise<void> => {
-    // Ask user to select a Pixel
-    const pixel = await Pixel.requestPixel();
-    setPixels((pixels) => {
-      if (!pixels.includes(pixel)) {
-        return [...pixels, pixel];
-      }
-      return pixels;
-    });
-    log("Connecting...");
-    await pixel.connect();
-  };
+  useEffect(() => {
+    async function loadProfiles() {
+      setDefaultAppDataSet(
+        await loadAppDataSetFromJson("default-profiles.json")
+      );
+      // const niceProfileAppDataSet = await loadAppDataSetFromJson(
+      //   "nice-profile.json"
+      // );
+      // setNiceProfileDataSet(
+      //   niceProfileAppDataSet
+      //     .extractForProfile(niceProfileAppDataSet.profiles[0])
+      //     .toDataSet()
+      // );
+    }
+    loadProfiles().catch((error) => console.error(error));
+  }, []);
 
   return (
     <div class={style.home}>
       <h1>Home</h1>
-      <p>Connected to {pixels.map((p) => p.name).join(", ")}</p>
-      <button onClick={connect}>Connect</button>
-      <p />
-      {pixels.map((pixel) => (
-        <PixelInfoBox key={pixel} pixel={pixel} />
-      ))}
+      {defaultAppDataSet ? (
+        <Demo defaultAppDataSet={defaultAppDataSet} />
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
